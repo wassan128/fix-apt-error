521f553f2a54f9d387d7ae1f7948be7566a909a3
wassan128 <wassan.taiko.master@gmail.com>
2019-07-07 00:18:29 +0900

    Add fix script

---
+++ fix.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+fix() {
+    if [[ -z $1 ]]; then
+        echo "usage: ${0##/*} target"
+        exit -1
+    fi
+
+    local basepath="/var/lib/dpkg/info"
+    for target in "$@"
+    do
+        sudo rm -f "$basepath/$target.postinst" \
+              "$basepath/$target.postrm" \
+              "$basepath/$target.preinst" &&
+
+        sudo dpkg --configure "$target"
+    done
+}
+
+fix $@
+
